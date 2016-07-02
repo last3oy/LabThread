6fe40d502bd00ff52dd815677d68cfa504c150ee
PSXIII <pisekpis@gmail.com>
2016-07-02 11:22:48 +0700

    193 Creating a HandlerThread

--- app/src/main/java/com/ps13/labthread/MainActivity.java
+++ app/src/main/java/com/ps13/labthread/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ps13.labthread;
 
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     Thread thread;
     Handler handler;
+
+    HandlerThread   backgroundHandlerThread;
+    Handler         backgroundHandler;
+    Handler         mainHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         */
 
         // Thread Method 3: Handler Only
+        /*
         handler = new Handler(Looper.getMainLooper()) {
             @Override
             public void handleMessage(Message msg) {
@@ -93,6 +99,40 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         handler.sendEmptyMessageDelayed(0, 1000);
+        */
+
+        // Thread Method 4: HandlerThread
+        backgroundHandlerThread = new HandlerThread("backgroundHandlerThread");
+        backgroundHandlerThread.start();
+
+        backgroundHandler = new Handler(backgroundHandlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // Run with background
+                Message msgMain = new Message();
+                msgMain.arg1 = msg.arg1 + 1;
+                mainHandler.sendMessage(msgMain);
+            }
+        };
+
+        mainHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // Run with Thread
+                tvCounter.setText(msg.arg1 + "");
+                if (msg.arg1 < 100) {
+                    Message msgBack = new Message();
+                    msgBack.arg1 = msg.arg1;
+                    backgroundHandler.sendMessageDelayed(msgBack, 1000);
+                }
+            }
+        };
+
+        Message msgBack = new Message();
+        msgBack.arg1 = 0;   // Start count at 0
+        backgroundHandler.sendMessageDelayed(msgBack, 1000);
     }
 
     @Override
@@ -100,5 +140,6 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         //thread.interrupt();
+        backgroundHandlerThread.quit();
     }
 }
